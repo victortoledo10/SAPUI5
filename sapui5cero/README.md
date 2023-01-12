2e6422504ba63231358bdc203b91cc17d136a4ec
Victor <victor@victor.com>
2023-01-12 08:46:41 +0100

    Añadiendo pantalla basica

--- sapui5cero/README.md
+++ sapui5cero/README.md
@@ -34,3 +34,6 @@ Demo
 1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
 
 
+Ejemplo:https://sapui5.hana.ondemand.com/#/topic/3da5f4be63264db99f2e5b04c5e853db
+
+URL servivio:https://sapes5.sapdevcenter.com/sap/opu/odata/sap/SEPMRA_PROD_MAN/
--- sapui5cero/webapp/controller/Main.controller.js
+++ sapui5cero/webapp/controller/Main.controller.js
@@ -1,15 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,MessageToast,JSONModel) {
         "use strict";
 
         return Controller.extend("es.victor.sapui5cero.controller.Main", {
             onInit: function () {
 
-            }
+                 // Crear odata
+                    var oData = {
+                        recipient : {
+                        name : "World"
+                        }
+                    };
+
+                    var oModel = new JSONModel(oData);
+                    this.getView().setModel(oModel);
+                    
+            //alert("UI5 is ready");  
+
+            },
+
+            //Evento desencadenado por el boton
+            onShowHello : function () {
+                // show a native JavaScript alert
+                MessageToast.show("Hello World");
+             }
+            
+
         });
     });
